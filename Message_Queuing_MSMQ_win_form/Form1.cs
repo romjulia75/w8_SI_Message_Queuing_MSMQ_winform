dbfe30bcc137a16f09b97d7c0624b7b711b6cf34
Roman Mashkovskyy <romjulia75@gmail.com>
2017-03-31 23:43:31 +0200

    no message

---
+++ Message_Queuing_MSMQ_win_form/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Message_Queuing_MSMQ_win_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.StockQuoteSoapClient scl = new ServiceReference1.StockQuoteSoapClient();
+
+           // string name = textBox1.Text;
+            string data = scl.GetQuote(textBox1.Text);
+
+            textBox2.Text = data;
+
+            }
+    }
+}
